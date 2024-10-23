09e8086571143f4c88866760d9fd4241c4734df6
mijinko3kaisei@gmail.com <mijinko3kaisei@gmail.com>
2024-10-23 12:29:52 +0900

    コンフリクト

--- main.cpp
+++ main.cpp
@@ -8,12 +8,17 @@ int main()
 
 	printf("Git Test2!\n");
 
-	printf("プレイヤーの移動処理追加\n");
+	printf("繝励Ξ繧､繝､繝ｼ縺ｮ遘ｻ蜍募・逅・ｿｽ蜉\n");
 
-	printf("プルリクエストのテスト\n");
+	printf("繝励Ν繝ｪ繧ｯ繧ｨ繧ｹ繝医・繝・せ繝・n");
 
-	printf("masterで変更\n");
 
+<<<<<<< HEAD
 	printf("ローカルで変更\n");
+=======
+	printf("繧ｳ繝ｳ繝励Μ繧ｯ繝医・繝・せ繝・n");
+  
+	printf("master縺ｧ螟画峩\n");
+>>>>>>> d8d4f31371e6f0b296c7f36d86e97da5ed3558c8
 	return 0;
 }
